6c98308f0cca23ba056728780c6744c5972df57d
zhenkaisitecore <zhenkai.wong@sitecore.com>
2020-12-16 15:18:12 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## ASP.NET Web Service (.asmx) Javascript Consumer Example
+
+The example of js consumer could be found in index.html
---
+++ WebServiceAttempt/MyWebService.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceAttempt
+{
+    /// <summary>
+    /// Summary description for MyWebService
+    /// </summary>
+    [WebService(Namespace = "wongzhenkai")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public Person GetPerson() => new Person() { Name = "wong zhen kai", Age = 24, Occupation = "Software Engineer Intern" };
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+    }
+}
